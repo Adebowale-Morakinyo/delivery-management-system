ff4e4972deca7860bd87a8aab4cbe3806c2006f2
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2024-09-07 09:05:46 +0100

    Proper Connection Handling in Peewee

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,5 +6,7 @@ db = PostgresqlDatabase(
     user=os.getenv('PGUSER', 'postgres'),
     password=os.getenv('PGPASSWORD', 'postgres'),
     host=os.getenv('PGHOST', 'localhost'),
-    port=int(os.getenv('PGPORT', 5432))
+    port=int(os.getenv('PGPORT', 5432)),
+    max_connections=20,
+    stale_timeout=300  # Close connections not used for 5 minutes
 )
