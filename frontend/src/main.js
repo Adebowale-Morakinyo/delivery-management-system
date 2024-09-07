8394d062f061c08fa45937580f296c923bffafe3
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2024-09-07 10:28:30 +0100

    Just some regular ui wonders

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import '@fortawesome/fontawesome-free/css/all.css'  // Importing Font Awesome CSS
 
 axios.defaults.baseURL = process.env.VUE_APP_API_URL
 
